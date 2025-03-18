6e9a51da2d23445bd1dda0f28acb0a183f6f2acb
0BooM <149960425+0BooM@users.noreply.github.com>
2025-03-18 23:05:15 +0100

    Create head(), tail(), at(index) methods

--- linked_lists/LinkedList.js
+++ linked_lists/LinkedList.js
@@ -1,27 +1,72 @@
 import Node from "./Node.js";
 
 export default class LinkedList {
-  constructor() {
-    this.listHead = null;
-  }
+    constructor() {
+        this.listHead = null;
+    }
+    // adds a new node containing value to the end of the list
+    append(value) {
+        let newNode = new Node(value);
 
-  append(value) {
-    let newNode = new Node(value);
+        if (!this.listHead) {
+            this.listHead = newNode;
+            return;
+        }
 
-    if (!this.listHead) {
-      this.listHead = newNode;
-      return;
+        let tmp = this.listHead;
+        while (tmp.nextNode) {
+            tmp = tmp.nextNode;
+        }
+        tmp.nextNode = newNode;
     }
 
-    let tmp = this.listHead;
-    while (tmp.nextNode) {
-      tmp = tmp.nextNode;
+    //adds a new node containing value to the start of the list
+    prepend(value) {
+        let newNode = new Node(value, this.listHead);
+        this.listHead = newNode;
     }
-    tmp.nextNode = newNode;
-  }
+    // returns the total number of nodes in the list
+    size() {
+        let counter = 0;
+        let tmp = this.listHead;
+        while (tmp) {
+            counter++;
+            tmp = tmp.nextNode;
+        }
 
-  prepend(value) {
-    let newNode = new Node(value, this.listHead);
-    this.listHead = newNode;
-  }
+        return counter;
+    }
+
+    // returns the first node in the list
+    head() {
+        return this.listHead;
+    }
+
+    tail(){
+        if (!this.listHead) return null;
+
+        let tmp = this.listHead;
+        while(tmp.nextNode){
+            tmp = tmp.nextNode;
+        }
+
+        return tmp;
+    }
+
+    at(index){
+        let counter = 0;
+        if (!this.listHead) return null; 
+        
+        let tmp = this.listHead;
+
+        while(tmp){
+            if(counter == index){
+                return tmp;
+            }
+
+            counter++;
+            tmp = tmp.nextNode;
+        }
+        return null;
+    }
 }
