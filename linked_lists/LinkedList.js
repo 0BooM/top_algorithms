c65b8d5d1d4ff9d38a07a97a58ec7c9fce952559
0BooM <149960425+0BooM@users.noreply.github.com>
2025-03-18 22:33:49 +0100

    Create append() method

--- linked_lists/LinkedList.js
+++ linked_lists/LinkedList.js
@@ -1,1 +1,20 @@
 import Node from "./Node";
+
+class LinkedList {
+  constructor() {
+    this.listHead = null;
+  }
+
+  append(value) {
+    if (!this.listHead) {
+      this.listHead = new Node(value);
+      return;
+    }
+
+    let tmp = this.listHead;
+    while (tmp.nextNode) {
+      tmp = tmp.nextNode;
+    }
+    tmp.nextNode = new Node(value);
+  }
+}
