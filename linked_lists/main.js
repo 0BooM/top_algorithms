d456839d29af8afb4a7364ccd5cae9c65e7d4f71
0BooM <149960425+0BooM@users.noreply.github.com>
2025-03-18 23:06:14 +0100

    test functions

--- linked_lists/main.js
+++ linked_lists/main.js
@@ -8,6 +8,12 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
+
+
 list.prepend("balbala");
 
 console.log(list);
+console.log(list.size());
+console.log(list.head());
+console.log(list.tail());
+console.log(list.at(2));
