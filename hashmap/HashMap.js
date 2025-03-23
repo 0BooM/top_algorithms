b91ecb8f7c213c6c8d333e3e15a328002d8cfac9
0BooM <149960425+0BooM@users.noreply.github.com>
2025-03-23 20:23:38 +0100

    Implement a hash(key) method, changed variable names

--- hashmap/HashMap.js
+++ hashmap/HashMap.js
@@ -1,7 +1,20 @@
 class HashMap{
     constructor(){
-        this.array = new Array[10].fill(null);
-        this.capacity = capacity;
+        this.bucketArray = new Array[10].fill(null);
+        this.capacity = this.array.length;
         this.loadFactor = 0.75;
     }
+
+    // method that takes a key and produces a hash code with it.
+    hash(key) {
+        let hashCode = 0;
+            
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    } 
+
 }
