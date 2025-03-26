c704d595b4bfa83decf91ef89761757c3bb8d153
0BooM <149960425+0BooM@users.noreply.github.com>
2025-03-26 15:39:09 +0100

    Create length() method

--- hashmap/HashMap.js
+++ hashmap/HashMap.js
@@ -1,5 +1,5 @@
-class HashMap{
-    constructor(){
+class HashMap {
+    constructor() {
         this.bucketArray = new Array(10).fill(null);
         this.capacity = this.bucketArray.length;
         this.loadFactor = 0.75;
@@ -8,38 +8,41 @@ class HashMap{
     // method that takes a key and produces a hash code with it.
     hash(key) {
         let hashCode = 0;
-            
+
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+            hashCode =
+                (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
 
         return hashCode;
-    } 
+    }
 
     //Sets new value at by calculating hashCode using key value
-    set(key, value){
+    set(key, value) {
         hashCode = this.hash(key);
-        
+
         this.bucketArray[hashCode] = value;
-        console.log(`Hashcode: ${hashedValue}, bucketArray[haschode]: ${this.bucketArray[hashedValue]} `)
+        console.log(
+            `Hashcode: ${hashedValue}, bucketArray[haschode]: ${this.bucketArray[hashedValue]} `
+        );
     }
 
     // returns value that is asigned to key
-    get(key){
+    get(key) {
         hashCode = this.hash(key);
 
         return this.bucketArray[hashCode] ? this.bucketArray[hashCode] : null;
     }
 
     // returns true or false based on whether passed key exists in array or not
-    has(key){
+    has(key) {
         hashCode = this.hash(key);
-        
+
         return this.bucketArray[hashCode] ? true : false;
     }
     //takes a key as an argument, then removes the entry with that key
-    remove(key){
+    remove(key) {
         if (!this.has(key)) return false;
 
         let hashCode = this.hash(key);
@@ -47,5 +50,10 @@ class HashMap{
         this.bucketArray[hashCode] = null;
 
         return true;
+    }
+
+    // returns the number of stored keys in the hash map
+    length() {
+        return this.bucketArray.filter((item) => item !== null).length;
     }
 }
